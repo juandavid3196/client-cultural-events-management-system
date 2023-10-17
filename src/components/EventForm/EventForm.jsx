82d5c5521503e5ea6ba192995ed327afa32b7edb
juan david rios <juandavid3196@gmail.com>
2023-10-17 14:52:49 -0500

    calculate duration, corrections form first part.

--- src/components/EventForm/EventForm.jsx
+++ src/components/EventForm/EventForm.jsx
@@ -1,54 +1,56 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../EventForm/EventForm.scss';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
-const EventForm = ({ onCloseForm, onFinishForm }) => {
+const EventForm = ({ onCloseForm, onFinishForm, updateItem, update, onUpdateState }) => {
 
 	const uuid = require('uuid');
 	const [section, setSection] = useState('evento');
 	const [close, setClose] = useState(false);
-
-	const [errors, setErrors] = useState({});
-
+	const [duration, setDuration] = useState('');
 	const [formData, setFormData] = useState({
 
 		id: uuid.v4(),
-		eventType: '',
-		state: '',
+		eventType: 'propio',
+		state: 'confirmado',
 		generalName: '',
 		specificName: '',
 		dateStart: '',
 		dateFinishing: '',
 		hourStart: '',
 		hourFinishing: '',
-		place: '',
+		place: 'sala',
 		userName: '',
 		phone: '',
-		identification: '',
+		identificationType: 'CC',
+		identificationValue: '',
 		email: '',
 		description: '',
-		duration: '',
+		observation: '',
+		duration: duration,
 		mountingDate: '',
-		mountingHour: '',
+		mountingStartHour: '',
+		mountingFinishingHour: '',
 		technicContact: '',
 		communicationContact: '',
-		pulep: '',
+		pulep: 'True',
 		accessData: '',
 		ticketCompany: '',
-		ageRestriction: '',
+		ageRestriction: 'False',
 
 	});
 
-	const validateForm = () => {
+	useEffect(() => {
+		console.log(formData);
+		calculateDuration();
+	}, [formData])
 
-		const newErrors = {};
-		const regex = /^[0-9]+$/;
-
-		if (regex.test(formData.phone)) {
-			newErrors.phone = true;
-		}
-		return newErrors;
-	};
+	useEffect(() => {
+		if (update) setFormData(updateItem);
+	}, [update, updateItem])
 
 	const handleInputChange = (e) => {
 
@@ -58,9 +60,6 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 			[name]: value,
 		});
 
-		const newErrors = validateForm();
-		setErrors(newErrors);
-
 	};
 
 
@@ -69,6 +68,7 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 		setTimeout(() => {
 			onCloseForm();
 		}, 500)
+		onUpdateState();
 	}
 
 	const resetFormData = () => {
@@ -85,30 +85,115 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 
 	const handleForm = (e) => {
 		e.preventDefault();
-		console.log(formData);
+
+		if (update) {
+			updateData(formData.id, formData);
+			toast.success('¡Evento Editado con Exito!');
+			onUpdateState();
+		} else {
+			postData(formData);
+			toast.success('¡Evento Añadido con Exito!');
+		}
 		resetFormData();
 		onFinishForm();
+	}
+
+	const postData = (newItem) => {
+		axios.post('http://localhost:5000/programming', newItem)
+			.then(response => {
+				console.log('Nuevo elemento creado:', response.data);
+			})
+			.catch(error => {
+				console.error('Error al crear el elemento:', error);
+			});
+	}
+
+	const updateData = (id, item) => {
+		axios.put(`http://localhost:5000/programming/${id}`, item)
+			.then(response => {
+				console.log('Elemento actualizado');
+			})
+			.catch(error => {
+				console.error('Error al actualizar el elemento:', error);
+			});
+
+	}
+
+	const identificationValue = (type) => {
+		if (type === 'CC') {
+			return '39180589';
+		} else if (type === 'RUT') {
+			return ' 123456789';
+		} else if (type === 'NIT') {
+			return '800.123.456-7';
+		}
+	}
+
+	const calculateDuration = () => {
+		let hours = 0;
+		let totalHours = 0;
+		let minutes = 0;
+		const StartHour = parseInt(`${formData.mountingStartHour[0]}${formData.mountingStartHour[1]}`);
+		const StartMinutes = parseInt(`${formData.mountingStartHour[3]}${formData.mountingStartHour[4]}`);
+		const finishingHour = parseInt(`${formData.mountingFinishingHour[0]}${formData.mountingFinishingHour[1]}`);
+		const finishingMinutes = parseInt(`${formData.mountingFinishingHour[3]}${formData.mountingFinishingHour[4]}`);
+
+
+		if (StartHour < finishingHour) {
+			hours = finishingHour - StartHour;
+
+			if (StartMinutes <= finishingMinutes) {
+				minutes = finishingMinutes - StartMinutes;
+			} else {
+				hours--;
+				minutes = (60 - StartMinutes) + finishingMinutes;
+			}
+			if (minutes > 60) {
+				totalHours = Math.floor(minutes / 60);
+				hours = hours + totalHours;
+				minutes = minutes % 60;
+			}
+		} else if (StartHour == finishingHour) {
+			hours = 0;
+			if (finishingMinutes > StartMinutes) {
+				minutes = finishingMinutes - StartMinutes;
+			}
+
+		} else {
+			hours = (23 - StartHour) + finishingHour;
+			minutes = (60 - StartMinutes) + finishingMinutes;
+
+			if (minutes >= 60) {
+				totalHours = Math.floor(minutes / 60);
+				hours = hours + totalHours;
+				minutes = minutes % 60;
+			}
+		}
+
+		let time = `${hours > 0 ? `${hours} Horas` : ''} ${minutes > 0 ? ` ${minutes} Minutos` : ''}`;
+
+		setDuration(time);
 	}
 
 	return (
 		<div className={`form-container ${close ? 'close' : ''}`}>
 			<div className={`form-main-box ${close ? 'close' : ''}`}>
 				<div className='form-title'>
-					<span>Añadir Evento</span>
+					<span>{update ? 'Editar Evento' : 'Añadir Evento'}</span>
 					<i class="fa-regular fa-circle-xmark" onClick={handleClose}></i>
 				</div>
 				<div className='main-form'>
 					<div className='form-eyeslashes'>
 						<span className={`eyeslashes-box ${section === 'evento' ? 'selected' : ''}`} onClick={() => setSection('evento')}>Evento</span>
 						<span className={`eyeslashes-box ${section === 'cliente' ? 'selected' : ''}`} onClick={() => setSection('cliente')}>Cliente</span>
-						<span className={`eyeslashes-box ${section === 'tecnica' ? 'selected' : ''}`} onClick={() => setSection('tecnica')}>Tecnica</span>
-						<span className={`eyeslashes-box ${section === 'comunicaciones' ? 'selected' : ''}`} onClick={() => setSection('comunicaciones')}>Comunicaciones</span>
+						<span className={`eyeslashes-box ${section === 'tecnica' ? 'selected' : ''}`} onClick={() => setSection('tecnica')}>Info Técnica</span>
+						<span className={`eyeslashes-box ${section === 'comunicaciones' ? 'selected' : ''}`} onClick={() => setSection('comunicaciones')}>Info Comunicaciones</span>
 					</div>
 					<form onSubmit={handleForm}>
 
 						{
 							section === 'evento' && (
-								<div className='form-section'>
+								<div className='form-section event-section'>
 									<span className='section-title'>Datos del Evento</span>
 									<div className="row two-colums">
 										<div className="form-box">
@@ -164,7 +249,7 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 											</div>
 										</div>
 									</div>
-									<div className="row">
+									<div className="row two-colums">
 										<div className="form-box">
 											<label htmlFor="place">Lugar</label>
 											<select name="place" onChange={handleInputChange} value={formData.place}>
@@ -174,6 +259,16 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 												<option value="aula-taller">Aula Taller</option>
 												<option value="otros">Otros</option>
 											</select>
+										</div>
+										<div className="form-box">
+											<label htmlFor="description">Descripción</label>
+											<input type="text" name='description' onChange={handleInputChange} value={formData.description} placeholder='Breve descripción del evento: Concierto, Obra Teatral, Exposición ' />
+										</div>
+									</div>
+									<div className="row">
+										<div className="form-box">
+											<label htmlFor="Observation">Observaciones</label>
+											<textarea name="observation" cols="30" rows="6" onChange={handleInputChange} value={formData.observation} placeholder='Observacion, Comentario, Indicación'></textarea>
 										</div>
 									</div>
 								</div>
@@ -186,30 +281,33 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 									<div className="row">
 										<div className="form-box">
 											<label htmlFor="userName">Nombre</label>
-											<input type="text" name='userName' onChange={handleInputChange} value={formData.userName} placeholder='Name' />
+											<input type="text" name='userName' onChange={handleInputChange} value={formData.userName} placeholder='Nombre' />
 										</div>
 									</div>
 									<div className="row two-colums">
 										<div className="form-box">
 											<label htmlFor="phone">Celular</label>
-											<input type="text" name='phone' onChange={handleInputChange} value={formData.phone} placeholder='Celular' />
-											{!errors.phone && formData.phone !== '' && (<span className='error-message'>Este campo solo permite numeros <i class="fa-solid fa-circle-xmark"></i></span>)}
+											<input type="text" name='phone' onChange={handleInputChange} value={formData.phone} placeholder='320890675 / 3108900823' />
 										</div>
-										<div className="form-box">
-											<label htmlFor="identification">Identificación</label>
-											<input type="text" name='identification' onChange={handleInputChange} value={formData.identification} placeholder='C.C, RUT, NIT' />
+										<div className="row two-colums-small">
+											<div className="form-box">
+												<label htmlFor="identificationType">Tipo de Identificación</label>
+												<select name="identificationType" onChange={handleInputChange} value={formData.identificationType}>
+													<option value="CC">Cedula</option>
+													<option value="RUT" >RUT</option>
+													<option value="NIT" >NIT</option>
+												</select>
+											</div>
+											<div className="form-box">
+												<label htmlFor="identificationValue">Numero de Indentificación</label>
+												<input type="text" name='identificationValue' onChange={handleInputChange} value={formData.identificationValue} placeholder={identificationValue(formData.identificationType)} />
+											</div>
 										</div>
 									</div>
 									<div className="row">
 										<div className="form-box">
 											<label htmlFor="email">Email</label>
 											<input type="email" name='email' onChange={handleInputChange} value={formData.email} placeholder='Correo Electronico' />
-										</div>
-									</div>
-									<div className="row">
-										<div className="form-box">
-											<label htmlFor="description">Descripción</label>
-											<input type="text" name='description' onChange={handleInputChange} value={formData.description} placeholder='Breve descripción del evento' />
 										</div>
 									</div>
 								</div>
@@ -219,24 +317,33 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 							section === 'tecnica' && (
 								<div className="form-section">
 									<span className='section-title'>Datos Tecnicos</span>
-									<div className="row three-colums">
-										<div className="form-box">
-											<label htmlFor="duration">Duracion</label>
-											<input type="number" name='duration' onChange={handleInputChange} value={formData.duration} min={0} max={24} placeholder='0 Horas' />
+									<div><span className='form-subtitle'>Fecha y Hora Montaje</span></div>
+									<div className="row two-colums">
+										<div className="row two-colums-small">
+											<div className="form-box">
+												<label htmlFor="duration">Duracion</label>
+												<input type="text" name='duration' disabled onChange={handleInputChange} value={duration} min={0} max={24} placeholder='0' />
+											</div>
+											<div className="form-box">
+												<label htmlFor="mountingDate">Fecha Montaje</label>
+												<input type="date" name='mountingDate' onChange={handleInputChange} value={formData.mountingDate} placeholder='dd/mm/yy' />
+											</div>
 										</div>
-										<div className="form-box">
-											<label htmlFor="mountingDate">Fecha Montaje</label>
-											<input type="date" name='mountingDate' onChange={handleInputChange} value={formData.mountingDate} placeholder='dd/mm/yy' />
-										</div>
-										<div className="form-box">
-											<label htmlFor="mountingHour">Hora Montaje</label>
-											<input type="time" name='mountingHour' onChange={handleInputChange} value={formData.mountingHour} placeholder='00:00' />
+										<div className="row two-colums-small">
+											<div className="form-box">
+												<label htmlFor="mountingStartHour">Hora Inicio </label>
+												<input type="time" name='mountingStartHour' onChange={handleInputChange} value={formData.mountingStartHour} placeholder='00:00' />
+											</div>
+											<div className="form-box">
+												<label htmlFor="mountingFinishigHour">Hora Finalización</label>
+												<input type="time" name='mountingFinishingHour' onChange={handleInputChange} value={formData.mountingFinishingHour} placeholder='00:00' />
+											</div>
 										</div>
 									</div>
 									<div className="row">
 										<div className="form-box">
 											<label htmlFor="technicContact">Contacto montaje tecnico</label>
-											<input type="text" name='technicContact' onChange={handleInputChange} value={formData.technicContact} placeholder='Nombre' />
+											<input type="text" name='technicContact' onChange={handleInputChange} value={formData.technicContact} placeholder='Nombre / Celular' />
 										</div>
 									</div>
 								</div>
@@ -273,7 +380,7 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 										<div className="form-box">
 											<label htmlFor="ageRestriction">Restriccion de edad</label>
 											<select name="ageRestriction" onChange={handleInputChange} value={formData.ageRestriction}>
-												<option value="NA" selected>No Aplica</option>
+												<option value="False">No Aplica</option>
 												<option value="10" >10</option>
 												<option value="11" >11</option>
 												<option value="12" >12</option>
@@ -288,7 +395,7 @@ const EventForm = ({ onCloseForm, onFinishForm }) => {
 									</div>
 									<div className="row">
 										<div className="form-box form-box-btn">
-											<button className='btn-send-form' type='submit'>Guardar</button>
+											<button className='btn-send-form' type='submit'>{update ? 'Editar' : 'Guardar'}</button>
 										</div>
 									</div>
 								</div>
--- src/components/events/Events.jsx
+++ src/components/events/Events.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../events/Events.scss';
-import { events } from "../../data/events.js";
 import EventForm from '../EventForm/EventForm';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const Events = () => {
 
 	const [openForm, setOpenForm] = useState(false);
 	const [openMenu, setOpenMenu] = useState(false);
+	const [events, setEvents] = useState([])
 	const [id, setId] = useState('');
+	const [updateItem, setUpdateItem] = useState({});
+	const [update, setUpdate] = useState(false);
+	const [searchTerm, setSearchTerm] = useState('');
+
+	useEffect(() => {
+		getEvents();
+	});
+
+	const filteredEvents = events.filter(event => event.generalName.toLowerCase().includes(searchTerm.toLowerCase()));
 
 	const closeForm = () => {
 		setOpenForm(false);
+	}
+
+	const handleAddEvent = () => {
+		setOpenForm(!openForm);
+		setOpenMenu(false);
 	}
 
 	const handleMenu = (id) => {
@@ -22,9 +39,59 @@ const Events = () => {
 		setOpenForm(false);
 	}
 
+	const getEvents = () => {
+		axios.get('http://localhost:5000/programming')
+			.then(response => {
+				setEvents(response.data);
+			})
+			.catch(error => {
+				console.error('Error al obtener datos:', error);
+			});
+	}
+
+	const deleteEvent = (eventId) => {
+		axios.delete(`http://localhost:5000/programming/${eventId}`)
+			.then(response => {
+				console.log('Elemento eliminado');
+			})
+			.catch(error => {
+				console.error('Error al eliminar el elemento:', error);
+			});
+		toast.success('¡Evento Eliminado con Exito!');
+	}
+
+	const updateEvent = (id) => {
+		getUnicItem(id);
+		setUpdate(true);
+		setOpenForm(true);
+		setOpenMenu(false);
+
+	}
+
+	const getUnicItem = (id) => {
+		axios.get(`http://localhost:5000/programming/${id}`)
+			.then(response => {
+				setUpdateItem(response.data);
+			})
+			.catch(error => {
+				console.error('Error al obtener el elemento:', error);
+			});
+	}
+
+	const UpdateState = () => {
+		setUpdate(false);
+	}
+
+
 	return (
 		<div className='container'>
-			{openForm && <EventForm onCloseForm={closeForm} onFinishForm={finishForm} />}
+			{openForm && <EventForm
+				onCloseForm={closeForm}
+				onFinishForm={finishForm}
+				updateItem={updateItem}
+				update={update}
+				onUpdateState={UpdateState}
+			/>}
 			<div className='section-title'>
 				<h3>Gestión de eventos</h3>
 			</div>
@@ -32,23 +99,23 @@ const Events = () => {
 				<div className='events-top'>
 					<div className='input-box'>
 						<div className="input-box-relative">
-							<input type="text" />
+							<input type="text" placeholder='Buscar' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
 							<i class="fa-solid fa-magnifying-glass"></i>
 						</div>
 					</div>
-					<button className='btn-add-event' onClick={() => setOpenForm(!openForm)}>Añadir evento</button>
+					<button className='btn-add-event' onClick={() => handleAddEvent()}>Añadir evento</button>
 				</div>
 				<div className='events-bottom'>
 					{
-						events.map((element, index) => (
+						filteredEvents.map((element, index) => (
 							<div key={index} className='event-box'>
 								{id === element.id && openMenu && <div className='event-options'>
 									<ul>
 										<li>Crear Sub-evento</li>
 										<li>Añadir Responsabilidad</li>
 										<li>Crear Carpetas</li>
-										<li>Editar</li>
-										<li>Eliminar</li>
+										<li onClick={() => updateEvent(element.id)}>Editar</li>
+										<li onClick={() => deleteEvent(element.id)}>Eliminar</li>
 									</ul>
 								</div>}
 								<div className="event-text">
@@ -65,6 +132,18 @@ const Events = () => {
 					}
 				</div>
 			</div>
+			<ToastContainer
+				position="top-right"
+				autoClose={2000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
 		</div>
 	)
 }
