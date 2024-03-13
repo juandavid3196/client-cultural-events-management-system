39c402c66b3ee43a0967983978cc08c9e2fe2629
juan david rios <juandavid3196@gmail.com>
2024-03-12 20:10:27 -0500

    Services folder deleted

--- src/Service/apiConfigs.js
+++
@@ -1,9 +0,0 @@
-const SERVER_URL = 'http://localhost:8007/api/responsability-by-mode?skip=0&limit=125';
-
-const API_ROUTES = {
-    responsabilitysModeId: `${SERVER_URL}`,
-};
-
-const fetcher = (url) => fetch(url).then((res) => res.json());
-
-export { SERVER_URL, API_ROUTES, fetcher };
