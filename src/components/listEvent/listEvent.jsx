d1762270452fc36351961ec8e566d6b83eca53f3
Arley <wilmerarleyd@gmail.com>
2024-05-03 12:10:58 -0500

    Fix Responsability visual

--- src/components/listEvent/listEvent.jsx
+++ src/components/listEvent/listEvent.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import TableSpecifyRespEvent from "../tableSpecifyRespEvent/tableSpecifyRespEvent";
 import Select from "react-select";
-import { toast, ToastContainer } from 'react-toastify';
+import { toast, ToastContainer } from "react-toastify";
 import "./listEvent.scss";
 
 const ListEvent = () => {
@@ -11,12 +11,12 @@ const ListEvent = () => {
   const [contractualMode, setContractualMode] = useState("");
   const [eventSpace, setEventSpace] = useState("");
   const [close, setClose] = useState(false);
-  const [name, setName] = useState('');
-  const [description, setDescription] = useState('');
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
   const [openForm, setOpenForm] = useState(false);
   const [openMenu, setOpenMenu] = useState(false);
   const [update, setUpdate] = useState(false);
-  const [id, setId] = useState('');
+  const [id, setId] = useState("");
   const [tablaKey, setTablaKey] = useState(0); // Nuevo estado para la clave de la tabla
 
   useEffect(() => {
@@ -74,58 +74,64 @@ const ListEvent = () => {
 
   const saveResponsability = async () => {
     try {
-      const response = await fetch(`http://localhost:8007/api/event-has-responsability/event/${selectedEvent.value}/specific-responsability`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name, description }),
-      });
+      const response = await fetch(
+        `http://localhost:8007/api/event-has-responsability/event/${selectedEvent.value}/specific-responsability`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ name, description }),
+        }
+      );
 
       if (response.ok) {
-        console.log('El recurso fue creado exitosamente.');
-        toast.success('¡Responsabilidad Añadida con Exito!');
-        setTablaKey(prevKey => prevKey + 1); // Incrementa la clave de la tabla para forzar la actualización
+        console.log("El recurso fue creado exitosamente.");
+        toast.success("¡Responsabilidad Añadida con Exito!");
+        setTablaKey((prevKey) => prevKey + 1); // Incrementa la clave de la tabla para forzar la actualización
       } else {
-        console.error('Hubo un problema al crear el recurso:', response.statusText);
+        console.error(
+          "Hubo un problema al crear el recurso:",
+          response.statusText
+        );
       }
     } catch (error) {
-      console.error('Hubo un error al realizar la solicitud:', error.message);
+      console.error("Hubo un error al realizar la solicitud:", error.message);
     }
     setUpdate(false);
     handleClose();
-  }
+  };
 
   const handleFormWindow = () => {
     setClose(false);
-    setName("")
-    setDescription("")
+    setName("");
+    setDescription("");
     setOpenForm(!openForm);
     setOpenMenu(false);
-  }
+  };
 
   const handleClose = () => {
     setClose(true);
     setOpenForm(false);
     setUpdate(false);
-    setId('');
-    handleSelectChange(selectedEvent)
-  }
+    setId("");
+    handleSelectChange(selectedEvent);
+  };
 
   const handleForm = (e) => {
     e.preventDefault();
     saveResponsability();
-  }
+  };
 
   const handleInputChangeName = (e) => {
     const { name, value } = e.target;
-    setName(value)
-  }
+    setName(value);
+  };
 
   const handleInputChangeDescription = (e) => {
     const { description, value } = e.target;
-    setDescription(value)
-  }
+    setDescription(value);
+  };
 
   return (
     <>
@@ -134,31 +140,40 @@ const ListEvent = () => {
           <h3>Cumplimiento de responsabilidades</h3>
         </div>
         <div className="listEvents-container">
-          <div className="list-top flex flex-col">
-            <div className="centered-container">
-              <div>
-                <h4><strong>Lista de Eventos</strong></h4>
-                <div className="input-box-relative">
-                  <Select
-                    options={eventos}
-                    value={selectedEvent}
-                    onChange={handleSelectChange}
-                    placeholder="Buscar evento"
-                    isSearchable
-                    styles={customStyles}
-                  />
-                </div>
+          <div className="list-top ">
+            <h4>
+              <strong>Lista de Eventos</strong>
+            </h4>
+            <div className="input-box">
+              <div className="input-box-relative">
+                <Select
+                  options={eventos}
+                  value={selectedEvent}
+                  onChange={handleSelectChange}
+                  placeholder="Buscar evento"
+                  isSearchable
+                  styles={customStyles}
+                />
               </div>
-              <button className='' onClick={() => handleFormWindow()}>Añadir Responsabilidad Especifica</button>
             </div>
-            <div className="flex flex-row justify-between">
-                <h4><strong>Modalidad contractual:</strong> {contractualMode}</h4>
-                <h4><strong>Espacio del evento:</strong> {eventSpace}</h4>
+            <div className="list-top-info">
+              <h4>
+                <strong>Modalidad contractual:</strong> {contractualMode}
+              </h4>
+              <h4>
+                <strong>Espacio del evento:</strong> {eventSpace}
+              </h4>
             </div>
+          <button className="btn-add-event" onClick={() => handleFormWindow()}>
+            Añadir Responsabilidad Especifica
+          </button>
           </div>
           <div className="table-style px-5" style={{ overflowY: "auto" }}>
             {selectedEvent && (
-              <TableSpecifyRespEvent key={tablaKey} eventId={selectedEvent.value} />
+              <TableSpecifyRespEvent
+                key={tablaKey}
+                eventId={selectedEvent.value}
+              />
             )}
           </div>
         </div>
@@ -177,29 +192,58 @@ const ListEvent = () => {
       </div>
 
       {openForm && (
-        <div className={`form-container ${close ? 'close' : ''}`}>
-          <div className={`form-main-box ${close ? 'close' : ''}`}>
-            <div className='form-title'>
-              <span> {update ? "Editar Responsabilidad" : "Crear Responsabilidad"} </span>
-              <i className="fa-regular fa-circle-xmark" onClick={() => handleClose()}></i>
+        <div className={`form-container ${close ? "close" : ""}`}>
+          <div className={`form-main-box ${close ? "close" : ""}`}>
+            <div className="form-title">
+              <span>
+                {" "}
+                {update
+                  ? "Editar Responsabilidad"
+                  : "Crear Responsabilidad"}{" "}
+              </span>
+              <i
+                className="fa-regular fa-circle-xmark"
+                onClick={() => handleClose()}
+              ></i>
             </div>
             <div className="main-form">
               <form onSubmit={handleForm}>
-                <div className='form-section event-section'>
-                  <span className='section-title'>Datos de la Responsabilidad Especifica</span>
+                <div className="form-section event-section">
+                  <span className="section-title">
+                    Datos de la Responsabilidad Especifica
+                  </span>
                   <div className="row">
                     <div className="form-box">
-                      <label htmlFor="responsability_name">Nombre de la responsabilidad Especifica</label>
-                      <input type="text" name='responsability_name' onChange={handleInputChangeName} value={name} placeholder='Nombre de la responsabilidad' />
+                      <label htmlFor="responsability_name">
+                        Nombre de la responsabilidad Especifica
+                      </label>
+                      <input
+                        type="text"
+                        name="responsability_name"
+                        onChange={handleInputChangeName}
+                        value={name}
+                        placeholder="Nombre de la responsabilidad"
+                      />
                     </div>
                     <div className="form-box">
-                      <label htmlFor="responsability_descriptión">Descripción de la responsabilidad Especifica</label>
-                      <input type="text" name='responsability_descriptión' onChange={handleInputChangeDescription} value={description} placeholder='Nombre de la responsabilidad' />
+                      <label htmlFor="responsability_descriptión">
+                        Descripción de la responsabilidad Especifica
+                      </label>
+                      <input
+                        type="text"
+                        name="responsability_descriptión"
+                        onChange={handleInputChangeDescription}
+                        value={description}
+                        placeholder="Nombre de la responsabilidad"
+                      />
                     </div>
                   </div>
                   <div className={`row ${update ? "two-colums" : ""}`}>
                     <div className="form-box form-box-btn">
-                      <button type="submit" className='btn-send-form'> {update ? "Editar" : "Guardar"} </button>
+                      <button type="submit" className="btn-send-form">
+                        {" "}
+                        {update ? "Editar" : "Guardar"}{" "}
+                      </button>
                     </div>
                   </div>
                 </div>
