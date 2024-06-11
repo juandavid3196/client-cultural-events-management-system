859ae14c9f2c60cfc556cb07c137e1f7af092301
Mateo Bedoya <mateo.bedoyao@udea.edu.co>
2024-06-10 22:39:35 -0500

    fixed any bug

--- src/components/Reports/EventsByModalities.jsx
+++ src/components/Reports/EventsByModalities.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
     Chart,
     Series,
@@ -13,171 +14,76 @@ import {
     Grid,
 } from 'devextreme-react/chart';
 
-const energySources = [
-    { value: 'Alquiler', name: 'Alquiler' },
-    { value: 'Propio', name: 'Propio' },
-    { value: 'Proyecto', name: 'Proyecto' },
-    { value: 'CoProducción', name: 'CoProducción' },
-    { value: 'Apoyo', name: 'Apoyo' },
-    { value: 'Canje', name: 'Canje' },
-];
-
-const countriesInfo = [
-    {
-        country: 'Enero',
-        Alquiler: 7,
-        Propio: 6,
-        Proyecto: 0,
-        CoProducción: 2,
-        Apoyo: 1,
-        Canje: 8
-    }, {
-        country: 'Febrero',
-        Alquiler: 7,
-        Propio: 2,
-        Proyecto: 3,
-        CoProducción: 9,
-        Apoyo: 1,
-        Canje: 1
-    }, {
-        country: 'Marzo',
-        Alquiler: 4,
-        Propio: 14,
-        Proyecto: 4,
-        CoProducción: 10,
-        Apoyo: 2,
-        Canje: 1
-    }, {
-        country: 'Abril',
-        Alquiler: 17,
-        Propio: 6,
-        Proyecto: 6,
-        CoProducción: 12,
-        Apoyo: 5,
-        Canje: 1
-    }, {
-        country: 'Mayo',
-        Alquiler: 1,
-        Propio: 8,
-        Proyecto: 2,
-        CoProducción: 2,
-        Apoyo: 3,
-        Canje: 4
-    }, {
-        country: 'Junio',
-        Alquiler: 5,
-        Propio: 11,
-        Proyecto: 9,
-        CoProducción: 8,
-        Apoyo: 17,
-        Canje: 5
-    },
-    {
-        country: 'Julio',
-        Alquiler: 16,
-        Propio: 10,
-        Proyecto: 16,
-        CoProducción: 7,
-        Apoyo: 15,
-        Canje: 0
-    },
-    {
-        country: 'Agosto',
-        Alquiler: 12,
-        Propio: 3,
-        Proyecto: 6,
-        CoProducción: 4,
-        Apoyo: 2,
-        Canje: 0
-    },
-    {
-        country: 'Septiembre',
-        Alquiler: 14,
-        Propio: 3,
-        Proyecto: 7,
-        CoProducción: 0,
-        Apoyo: 4,
-        Canje: 1
-    },
-    {
-        country: 'Octubre',
-        Alquiler: 21,
-        Propio: 1,
-        Proyecto: 4,
-        CoProducción: 0,
-        Apoyo: 2,
-        Canje: 0
-    },
-    {
-        country: 'Noviembre',
-        Alquiler: 0,
-        Propio: 1,
-        Proyecto: 14,
-        CoProducción: 12,
-        Apoyo: 15,
-        Canje: 2
-    },
-    {
-        country: 'Diciembre',
-        Alquiler: 4,
-        Propio: 4,
-        Proyecto: 3,
-        CoProducción: 5,
-        Apoyo: 17,
-        Canje: 1
-    }
-];
-
 const EventsByModalities = () => {
     const [selectedMode, setSelectedMode] = useState('Todos');
+    const [eventsInfo, setEventsInfo] = useState([]);
+    const [modeTypes, setModeTypes] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8007/api/events/count/modes?year=2024');
+                if (response.status === 200) {
+                    const data = response.data;
+                    setEventsInfo(data.events_by_mode_info);
+                    setModeTypes(data.mode_types);
+                } else {
+                    console.error(`Error en la petición: ${response.status}`);
+                }
+            } catch (error) {
+                console.error('Error al hacer la petición:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
-        
-            <div className="bg-white rounded-lg p-8 shadow-md w-5/6">
-                <select
-                    value={selectedMode}
-                    onChange={(e) => setSelectedMode(e.target.value)}
+        <div className="bg-white rounded-lg p-8 shadow-md w-5/6">
+            <select
+                value={selectedMode}
+                onChange={(e) => setSelectedMode(e.target.value)}
+            >
+                <option value="Todos">Todos</option>
+                {modeTypes.map((source) => (
+                    <option key={source.value} value={source.value}>
+                        {source.name}
+                    </option>
+                ))}
+            </select>
+            <Chart palette="Violet" dataSource={eventsInfo}>
+                <CommonSeriesSettings
+                    argumentField="month"
+                    type="line"
+                />
+                {selectedMode === 'Todos'
+                    ? modeTypes.map((item) => (
+                        <Series
+                            key={item.value}
+                            valueField={item.value}
+                            name={item.name}
+                        />
+                    ))
+                    : <Series valueField={selectedMode} name={selectedMode} />}
+                <Margin bottom={20} />
+                <ArgumentAxis
+                    valueMarginsEnabled={false}
+                    discreteAxisDivisionMode="crossLabels"
                 >
-                    <option value="Todos">Todos</option>
-                    {energySources.map((source) => (
-                        <option key={source.value} value={source.value}>
-                            {source.name}
-                        </option>
-                    ))}
-                </select>
-                <Chart palette="Violet" dataSource={countriesInfo}>
-                    <CommonSeriesSettings
-                        argumentField="country"
-                        type="line"
-                    />
-                    {selectedMode === 'Todos'
-                        ? energySources.map((item) => (
-                            <Series
-                                key={item.value}
-                                valueField={item.value}
-                                name={item.name}
-                            />
-                        ))
-                        : <Series valueField={selectedMode} name={selectedMode} />}
-                    <Margin bottom={20} />
-                    <ArgumentAxis
-                        valueMarginsEnabled={false}
-                        discreteAxisDivisionMode="crossLabels"
-                    >
-                        <Grid visible={true} />
-                    </ArgumentAxis>
-                    <Legend
-                        verticalAlignment="bottom"
-                        horizontalAlignment="center"
-                        itemTextPosition="bottom"
-                    />
-                    <Export enabled={true} />
-                    <Title text="Eventos por modalidad contractual">
-                        <Subtitle text="Total eventos de cada modalidad por mes" />
-                    </Title>
-                    <Tooltip enabled={true} />
-                </Chart>
-            </div>
+                    <Grid visible={true} />
+                </ArgumentAxis>
+                <Legend
+                    verticalAlignment="bottom"
+                    horizontalAlignment="center"
+                    itemTextPosition="bottom"
+                />
+                <Export enabled={true} />
+                <Title text="Eventos por modalidad contractual">
+                    <Subtitle text="Total eventos de cada modalidad por mes" />
+                </Title>
+                <Tooltip enabled={true} />
+            </Chart>
+        </div>
     );
 };
 
--- src/components/Reports/EventsBySpaces.jsx
+++ src/components/Reports/EventsBySpaces.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import {
     Chart,
     Series,
@@ -13,159 +14,79 @@ import {
     Grid,
 } from 'devextreme-react/chart';
 
-const spaceTypes = [
-    { value: 'Sala', name: 'Sala' },
-    { value: 'CafeTeatro', name: 'Cafe Teatro' },
-    { value: 'Plazoleta', name: 'Plazoleta' },
-    { value: 'AulaTaller', name: 'Aula Taller' },
-    { value: 'Otros', name: 'Otros' },
-];
-
-const eventsBySpacesInfo = [
-    {
-        month: 'Enero',
-        Sala: 7,
-        CafeTeatro: 6,
-        Plazoleta: 0,
-        AulaTaller: 2,
-        Otros: 1,
-    }, {
-        month: 'Febrero',
-        Sala: 7,
-        CafeTeatro: 2,
-        Plazoleta: 3,
-        AulaTaller: 9,
-        Otros: 1,
-    }, {
-        month: 'Marzo',
-        Sala: 4,
-        CafeTeatro: 14,
-        Plazoleta: 4,
-        AulaTaller: 10,
-        Otros: 2,
-    }, {
-        month: 'Abril',
-        Sala: 17,
-        CafeTeatro: 6,
-        Plazoleta: 6,
-        AulaTaller: 12,
-        Otros: 5,
-    }, {
-        month: 'Mayo',
-        Sala: 1,
-        CafeTeatro: 8,
-        Plazoleta: 2,
-        AulaTaller: 2,
-        Otros: 3,
-    }, {
-        month: 'Junio',
-        Sala: 5,
-        CafeTeatro: 11,
-        Plazoleta: 9,
-        AulaTaller: 8,
-        Otros: 17,
-    },
-    {
-        month: 'Julio',
-        Sala: 16,
-        CafeTeatro: 10,
-        Plazoleta: 16,
-        AulaTaller: 7,
-        Otros: 15,
-    },
-    {
-        month: 'Agosto',
-        Sala: 12,
-        CafeTeatro: 3,
-        Plazoleta: 6,
-        AulaTaller: 4,
-        Otros: 2,
-    },
-    {
-        month: 'Septiembre',
-        Sala: 14,
-        CafeTeatro: 3,
-        Plazoleta: 7,
-        AulaTaller: 0,
-        Otros: 4,
-    },
-    {
-        month: 'Octubre',
-        Sala: 21,
-        CafeTeatro: 1,
-        Plazoleta: 4,
-        AulaTaller: 0,
-        Otros: 2,
-    },
-    {
-        month: 'Noviembre',
-        Sala: 0,
-        CafeTeatro: 1,
-        Plazoleta: 14,
-        AulaTaller: 12,
-        Otros: 15,
-    },
-    {
-        month: 'Diciembre',
-        Sala: 4,
-        CafeTeatro: 4,
-        Plazoleta: 3,
-        AulaTaller: 5,
-        Otros: 17,
-    }
-];
 
 const EventsBySpaces = () => {
     const [selectedSpace, setSelectedSpace] = useState('Todos');
+    const [eventsBySpacesInfo, setEventsBySpacesInfo] = useState([]);
+    const [spaceTypes, setSpaceTypes] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:8007/api/events/count/spaces?year=2024');
+                if (response.status === 200) {
+                    const data = response.data;
+                    setEventsBySpacesInfo(data.events_by_spaces_info);
+                    setSpaceTypes(data.space_types);
+                } else {
+                    console.error(`Error en la petición: ${response.status}`);
+                }
+            } catch (error) {
+                console.error('Error al hacer la petición:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
-        
-            <div className="bg-white rounded-lg p-8 shadow-md w-5/6">
-                <select
-                    value={selectedSpace}
-                    onChange={(e) => setSelectedSpace(e.target.value)}
+
+        <div className="bg-white rounded-lg p-8 shadow-md w-5/6">
+            <select
+                value={selectedSpace}
+                onChange={(e) => setSelectedSpace(e.target.value)}
+            >
+                <option value="Todos">Todos</option>
+                {spaceTypes.map((space) => (
+                    <option key={space.value} value={space.value}>
+                        {space.name}
+                    </option>
+                ))}
+            </select>
+            <Chart palette="Violet" dataSource={eventsBySpacesInfo}>
+                <CommonSeriesSettings
+                    argumentField="month"
+                    type="line"
+                />
+                {selectedSpace === 'Todos'
+                    ? spaceTypes.map((item) => (
+                        <Series
+                            key={item.value}
+                            valueField={item.value}
+                            name={item.name}
+                        />
+                    ))
+                    : <Series valueField={selectedSpace} name={selectedSpace} />}
+                <Margin bottom={20} />
+                <ArgumentAxis
+                    valueMarginsEnabled={false}
+                    discreteAxisDivisionMode="crossLabels"
                 >
-                    <option value="Todos">Todos</option>
-                    {spaceTypes.map((space) => (
-                        <option key={space.value} value={space.value}>
-                            {space.name}
-                        </option>
-                    ))}
-                </select>
-                <Chart palette="Violet" dataSource={eventsBySpacesInfo}>
-                    <CommonSeriesSettings
-                        argumentField="month"
-                        type="line"
-                    />
-                    {selectedSpace === 'Todos'
-                        ? spaceTypes.map((item) => (
-                            <Series
-                                key={item.value}
-                                valueField={item.value}
-                                name={item.name}
-                            />
-                        ))
-                        : <Series valueField={selectedSpace} name={selectedSpace} />}
-                    <Margin bottom={20} />
-                    <ArgumentAxis
-                        valueMarginsEnabled={false}
-                        discreteAxisDivisionMode="crossLabels"
-                    >
-                        <Grid visible={true} />
-                    </ArgumentAxis>
-                    <Legend
-                        verticalAlignment="bottom"
-                        horizontalAlignment="center"
-                        itemTextPosition="bottom"
-                    />
-                    <Export enabled={true} />
-                    <Title text="Eventos por espacio">
-                        <Subtitle text="Total eventos por cada espacio" />
-                    </Title>
-                    <Tooltip enabled={true} />
-                </Chart>
-            </div>
-        
+                    <Grid visible={true} />
+                </ArgumentAxis>
+                <Legend
+                    verticalAlignment="bottom"
+                    horizontalAlignment="center"
+                    itemTextPosition="bottom"
+                />
+                <Export enabled={true} />
+                <Title text="Eventos por espacio">
+                    <Subtitle text="Total eventos por cada espacio" />
+                </Title>
+                <Tooltip enabled={true} />
+            </Chart>
+        </div>
+
     );
 };
 
